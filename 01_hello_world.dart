ac4403a8552021ffa7927b5ded903385e9e0960d
Alex Hernandez Rios <84199764+AlexhHr23@users.noreply.github.com>
2024-07-08 11:15:38 -0600

    01_hello_world

---
+++ 01_hello_world.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  //var myName = 'Alex';
+  //late final myName;
+  //String myName = 'Alex';
+ 
+  const myName = 'Alex';
+ 
+  print('Hola $myName');
+
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+  
+  
+}
